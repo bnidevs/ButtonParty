267e4fff794e8bc696fe2ed0483347aad6f21e62
Brandon George <georgb3@rpi.edu>
2021-10-04 15:38:59 -0400

    Need help debugging
    Got rid of all errors but neither points nor streak variable is go up

--- ButtonParty/App.js
+++ ButtonParty/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import { TouchableHighlight, StyleSheet, View, Text, Button } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const AppButton = ({ onPress, title }) => {
   return (
@@ -15,19 +16,62 @@ const AppButton = ({ onPress, title }) => {
   );
 };
 
-const App = () => {
+export default function App() {
+  const [streak, setStreak] = useState('0');
   const [points, setPoints] = useState(0);
-  const onPress = () => (
-    //Do database stuff here
-    setPoints(prevCount => prevCount + 1)
-  );
+
+  const add = async () => {
+    try {
+      await AsyncStorage.setItem('streak', streak);
+    } catch (e) {
+      console.log("Unable to save data");
+    }
+  }
+
+  const getStreak = async () => {
+    try {
+      const val = await AsyncStorage.getItem('streak');
+      if(val != null) {
+        setStreak(val);
+      } 
+    } catch (e) {
+      console.log("Unable to read streak data");
+    }
+  }
+
+  const clear = async () => {
+    try {
+      AsyncStorage.clear();
+    } catch(e) {
+      console.log("Could not clear");
+    }
+  }
+
+  const onPress = () => {
+    try {
+      getStreak();
+      setStreak(JSON.stringify(parseInt(streak) + 1));
+      let temp_streak = parseInt(streak);
+      console.log(temp_streak);
+      if(temp_streak <= 10) {
+        setPoints(points + temp_streak);
+      } else {
+        setPoints(points + Math.pow(temp_streak, 2));
+      }
+      add();
+    } catch (e) {
+      console.log("crash");
+    }
+      
+  }
 
   return (
     <View style = {styles.container}>
       <AppButton onPress={onPress} title="Button Party"/>
-      <Text style={styles.points}>{points ? points : null}</Text>
+      <Text style={styles.bottomText}>Points: {points}</Text>
+      <Text style={styles.bottomText}>Streak: {streak}</Text>
     </View>
- )
+ );
 }
 
 const styles = StyleSheet.create({
@@ -53,15 +97,15 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     textTransform: "uppercase"
   },
-  points: {
+  bottomText: {
     paddingTop: 50,
     fontSize: 18,
     color: "#000",
     fontWeight: "bold",
     alignSelf: "center",
     textTransform: "uppercase"
+  },
+  clear: {
+    
   }
 });
-
-
-export default App
