422deefba3ac721e4cc0f74ed0a61d54131f0611
Brandon George <georgb3@rpi.edu>
2021-10-05 16:13:10 -0400

    Updated App.js for the react-native-cli folder and deleted the extra folder

--- ButtonPartyTest/App.js
+++ ButtonPartyTest/App.js
@@ -1,112 +1,114 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { TouchableHighlight, StyleSheet, View, Text, Button } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
+  //Setting up hook variables for streak and points
+  let [streak, setStreak] = useState('0');
+  let [points, setPoints] = useState(0);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  //Sets the streak key in local storage to current streak value after button press
+  const set = async (val) => {
+    try {
+      await AsyncStorage.setItem('streak', val);
+    } catch (e) {
+      console.log("Unable to save data");
+    }
+  }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  //Fetches the current streak value
+  //Sets variable "streak" to stored val
+  const getStreak = async () => {
+    try {
+      const val = await AsyncStorage.getItem('streak');
+      if(val != null) {
+        setStreak(val);
+      } 
+    } catch (e) {
+      console.log("Unable to read streak data");
+    }
+  }
+
+  //Clears the stored data, added this for testing 
+  //also could be used if user deletes account
+  const clear = async () => {
+    try {
+      await AsyncStorage.clear();
+      setStreak('0');
+    } catch(e) {
+      console.log("Could not clear");
+    }
+  }
+
+  //Called when button is pressed, handles points increment
+  function onPress() {
+    getStreak();
+    let temp_streak = parseInt(streak) + 1;
+    if(temp_streak <= 10) {
+      setPoints(points + temp_streak);
+    } else {
+      setPoints(points + Math.pow(temp_streak, 2));
+    }
+    set(temp_streak.toString());   
+  }
+
+  //Custom button component
+  const AppButton = ({ onPress, title }) => {
+    return (
+      <TouchableHighlight 
+        activeOpacity={.8}
+        underlayColor="#db0000"
+        onPress={onPress}
+        style={styles.appButtonContainer}
+      >
+        <Text style={styles.appButtonText}>{title}</Text>
+      </TouchableHighlight>
+    );
+  }
+
+  //Get the streak value before return so it prints the correct streak
+  getStreak();
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style = {styles.container}>
+      <AppButton onPress={onPress} title="Button Party"/>
+      <Text style={styles.bottomText}>Points: {points}</Text>
+      <Text style={styles.bottomText}>Streak: {streak}</Text>
+      <Button style={styles.buttomButton} onPress={clear} title="Clear"/>
+      <Button style={styles.buttomButton} onPress={() => setPoints(0)} title="Reset Points"/>
     </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+);
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#c2c2c2',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  appButtonContainer: {
+    backgroundColor: "#ff0000",
+    width: 390,
+    height: 390,
+    borderRadius: 390,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
   },
-  sectionDescription: {
-    marginTop: 8,
+  appButtonText: {
+    fontSize: 30,
+    color: "#000",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+  bottomText: {
+    paddingTop: 50,
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    color: "#000",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
   },
 });
-
-export default App;
