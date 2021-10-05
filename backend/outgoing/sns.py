d8c51d961483e6c5dcf4c07eaac65ecdb74c1a13
pinkoa2 <pinkoa2.rpi.edu>
2021-10-05 15:34:28 -0400

    SQS Scoring pull and deletes

--- backend/outgoing/sns.py
+++ backend/outgoing/sns.py
@@ -1,23 +1,11 @@
 import sys
 import os
 sys.path.append(os.path.abspath('../'))
-import random
-import time
 
-from constants import SNS_ARN, SNS_CLIENT, BUTTON_FREQUENCY
-
-def run_button_game():
-    while(True):
-        time.sleep(1)
-        if( random.random() < 1 / BUTTON_FREQUENCY ):
-            print('Sending out SNS notification')
-            send_message_to_SNS('PRESS BUTTON')
+from constants import SNS_ARN, SNS_CLIENT
 
 def send_message_to_SNS( message ):
     SNS_CLIENT.publish(
         TargetArn=SNS_ARN,
         Message=message
     )
-
-if __name__ == '__main__':
-    run_button_game()
