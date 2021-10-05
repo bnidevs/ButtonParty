a8f3c1e4342788f22d732fdac0bd6e755a817d00
Brandon George <georgb3@rpi.edu>
2021-10-05 12:04:44 -0400

    Local storage of user streak functional

--- ButtonParty/App.js
+++ ButtonParty/App.js
@@ -1,28 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { TouchableHighlight, StyleSheet, View, Text, Button } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const AppButton = ({ onPress, title }) => {
-  return (
-    <TouchableHighlight 
-      activeOpacity={.8}
-      underlayColor="#db0000"
-      onPress={onPress}
-      style={styles.appButtonContainer}
-    >
-      <Text style={styles.appButtonText}>{title}</Text>
-    </TouchableHighlight>
-  );
-};
-
 export default function App() {
-  const [streak, setStreak] = useState('0');
-  const [points, setPoints] = useState(0);
 
-  const add = async () => {
+  let [streak, setStreak] = useState('0');
+  let [points, setPoints] = useState(0);
+
+  const set = async (val) => {
     try {
-      await AsyncStorage.setItem('streak', streak);
+      await AsyncStorage.setItem('streak', val);
     } catch (e) {
       console.log("Unable to save data");
     }
@@ -41,37 +29,47 @@ export default function App() {
 
   const clear = async () => {
     try {
-      AsyncStorage.clear();
+      await AsyncStorage.clear();
+      setStreak('0');
     } catch(e) {
       console.log("Could not clear");
     }
   }
 
-  const onPress = () => {
-    try {
-      getStreak();
-      setStreak(JSON.stringify(parseInt(streak) + 1));
-      let temp_streak = parseInt(streak);
-      console.log(temp_streak);
-      if(temp_streak <= 10) {
-        setPoints(points + temp_streak);
-      } else {
-        setPoints(points + Math.pow(temp_streak, 2));
-      }
-      add();
-    } catch (e) {
-      console.log("crash");
+  function onPress() {
+    getStreak();
+    let temp_streak = parseInt(streak) + 1;
+    if(temp_streak <= 10) {
+      setPoints(points + temp_streak);
+    } else {
+      setPoints(points + Math.pow(temp_streak, 2));
     }
-      
+    set(temp_streak.toString());   
   }
 
+  const AppButton = ({ onPress, title }) => {
+    return (
+      <TouchableHighlight 
+        activeOpacity={.8}
+        underlayColor="#db0000"
+        onPress={onPress}
+        style={styles.appButtonContainer}
+      >
+        <Text style={styles.appButtonText}>{title}</Text>
+      </TouchableHighlight>
+    );
+  }
+
+
+  getStreak();
   return (
     <View style = {styles.container}>
       <AppButton onPress={onPress} title="Button Party"/>
       <Text style={styles.bottomText}>Points: {points}</Text>
       <Text style={styles.bottomText}>Streak: {streak}</Text>
+      <Button onPress={clear} title="Clear"/>
     </View>
- );
+);
 }
 
 const styles = StyleSheet.create({
@@ -83,15 +81,15 @@ const styles = StyleSheet.create({
   },
   appButtonContainer: {
     backgroundColor: "#ff0000",
-    width: 250,
-    height: 250,
-    borderRadius: 250,
+    width: 390,
+    height: 390,
+    borderRadius: 390,
     alignItems: 'center',
     justifyContent: 'center',
     elevation: 8,
   },
   appButtonText: {
-    fontSize: 18,
+    fontSize: 30,
     color: "#000",
     fontWeight: "bold",
     alignSelf: "center",
