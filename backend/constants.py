1151c8b697d071301d326180b6b2c3b172db6dce
pinkoa2 <pinkoa2.rpi.edu>
2021-10-08 13:20:25 -0400

    timestamp added

--- backend/constants.py
+++ backend/constants.py
@@ -6,11 +6,20 @@ from boto3 import client
 # Button frequency : 1/ BUTTON_FREQUENCY
 BUTTON_FREQUENCY = 10000
 
+# In case of delay, we give a small buffer to the time to press
+TIME_TO_PRESS_BUFFER = 10
+
+# The user has the press the button within a certain amount of time
+TIME_TO_PRESS = 100 + TIME_TO_PRESS_BUFFER
+
 # URL of the SQS queue ButtonParty-Incoming
 SQS_INCOMING_QUEUE_URL = 'INSERT SQS incoming URL'
 
 # URL of the SQS queue ButtonParty-Scoring
-SQS_SCORING_QUEUE_URL = 'INSERT SQS Scoring URL'
+SQS_SCORING_QUEUE_URL = ''
+
+# URL of the SQS ButtonParty-check_timestamp
+SQS_TIMESTAMP_QUEUE_URL = ''
 
 # AWS Region
 REGION = 'us-east-1'
@@ -19,10 +28,10 @@ REGION = 'us-east-1'
 DATABASE_NAME = 'users'
 
 # RDS_RESOURCE_ARN
-RDS_RESOURCE_ARN = 'INSERT RDS RESOURCE ARN'
+RDS_RESOURCE_ARN = ''
 
 # RDS SECRET ARN
-RDS_SECRET_ARN = 'INSERT RDS SECRET ARN'
+RDS_SECRET_ARN = ''
 
 # SNS ARN
 SNS_ARN = 'INSERT SNS ARN'
--- backend/outgoing/game_logic.py
+++ backend/outgoing/game_logic.py
@@ -6,23 +6,57 @@ import random
 import time
 import json
 
-from constants import BUTTON_FREQUENCY, SQS_SCORING_QUEUE_URL
+from constants import BUTTON_FREQUENCY, TIME_TO_PRESS, SQS_SCORING_QUEUE_URL, SQS_TIMESTAMP_QUEUE_URL
 from rds import get_user_from_RDS, update_user_in_RDS
-from sqs import receive_messages_from_SQS, delete_messages_from_SQS
+from sqs import receive_messages_from_SQS, delete_messages_from_SQS, purge_queue_from_SQS, send_message_to_SQS
 from helper import validateJson
+
+timestamp_button_limit = None
+
+def call_events():
+    global timestamp_button_limit
+    timestamp_button_limit = None
+    print('Button Activated')
+    # Clear out the SQS TIMESTAMP QUEUE
+    purge_queue_from_SQS( SQS_TIMESTAMP_QUEUE_URL )
+    # Send out a message to the SQS TIMESTAMP DATABASE
+    send_message_to_SQS( 'PRESS BUTTON', SQS_TIMESTAMP_QUEUE_URL )
+    # Send out push notification to users
+    send_message_to_SNS('PRESS BUTTON')
 
 def run_button_game():
     while(True):
         time.sleep(1)
         if( random.random() < 1 / BUTTON_FREQUENCY ):
-            print('Sending out SNS notification')
-            send_message_to_SNS('PRESS BUTTON')
+            call_events()
 
-def increment_score(username, streak):
+def get_timestamp():
+    global timestamp_button_limit
+    if(timestamp_button_limit == None):
+        try:
+            print('here')
+            message = receive_messages_from_SQS( SQS_TIMESTAMP_QUEUE_URL )[0]
+            timestamp_button_limit = int(message['Attributes']['SentTimestamp'])
+        except Exception as err:
+            timestamp_button_limit = None
+    return timestamp_button_limit
+
+def is_valid_timestamp( timestamp_from_request ):
+    timestamp = get_timestamp()
+    user_time_to_press = (timestamp_from_request - timestamp) / 1000
+    if( user_time_to_press <= TIME_TO_PRESS ):
+        return True
+    return False
+
+def increment_score(username, streak, player_timestamp):
     try:
         user = get_user_from_RDS( username )
     except Exception as err:
         print('Unable to get User', err)
+        return
+
+    if( not is_valid_timestamp( player_timestamp )):
+        print('Button pressed after acceptable time limit')
         return
 
     # If the streak, is zero, no points are added No need to check for cheating
@@ -72,11 +106,12 @@ def check_the_pressed_buttons():
             'Id': message['MessageId'],
             'ReceiptHandle': message['ReceiptHandle']
         })
+        player_timestamp = int(message['Attributes']['SentTimestamp'])
         try:
             body = json.loads(message['Body'])
         except Exception as err:
             body = None
         if(validateJson(body)):
-            increment_score( body['username'], body['streak']);
+            increment_score( body['username'], body['streak'], player_timestamp);
     delete_messages_from_SQS( receiptHandles, SQS_SCORING_QUEUE_URL )
     return True
