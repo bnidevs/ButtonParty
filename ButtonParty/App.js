b28710733ee249e6d58779c48af00f146b74df9b
Bill Ni <bill.8ni@gmail.com>
2021-11-09 17:07:19 -0500

    update app.js for merge compat with #22

--- ButtonParty/App.js
+++ ButtonParty/App.js
@@ -1,80 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component, useState, useEffect } from 'react';
-import { TouchableHighlight, StyleSheet, View, Text, Button } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { TouchableHighlight, ImageBackground, StyleSheet, View, Text, Button, Alert } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import messaging from '@react-native-firebase/messaging';
+import notifee, { EventType } from '@notifee/react-native';
 
 export default function App() {
   //Setting up hook variables for streak and points
-  let [streak, setStreak] = useState('0');
+  let [streak, setStreak] = useState(0);
   let [points, setPoints] = useState(0);
+  let [active, setActive] = useState(false);
+  let username = "bdngeorge";
 
-  //Sets the streak key in local storage to current streak value after button press
-  const set = async (val) => {
-    try {
-      await AsyncStorage.setItem('streak', val);
-    } catch (e) {
-      console.log("Unable to save data");
-    }
+  async function onMessageReceived(message) {
+    // Create a channel
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: '<p style="color:red;"><b>Press the Button</b></p>',
+      body: '<em>You have 1 minute to press the button!</em>',
+      android: {
+        channelId : 'default',
+          autoCancel : true,
+          pressAction: {
+              id: "default",
+          },
+      }
+    });
   }
 
-  //Fetches the current streak value
-  //Sets variable "streak" to stored val
-  const getStreak = async () => {
-    try {
-      const val = await AsyncStorage.getItem('streak');
-      if(val != null) {
-        setStreak(val);
-      } 
-    } catch (e) {
-      console.log("Unable to read streak data");
+  notifee.onBackgroundEvent(async ({ type, detail }) => {
+    if (type === EventType.PRESS) {
+      console.log('User pressed the notification.', detail.pressAction.id);
     }
-  }
+  });
+  useEffect(() => {
+    const foreground = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage['data']['message']));
+      setActive(true);
+    });
 
-  //Clears the stored data, added this for testing 
-  //also could be used if user deletes account
-  const clear = async () => {
-    try {
-      await AsyncStorage.clear();
-      setStreak('0');
-    } catch(e) {
-      console.log("Could not clear");
-    }
-  }
+    return foreground;
+  }, []);
+  useEffect(() => {
+    const background = messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', JSON.stringify(remoteMessage['data']['message']));
+      onMessageReceived(remoteMessage);
+      setActive(true);
+    });
+
+    return background;
+  }, []);
 
   //Called when button is pressed, handles points increment
-  function onPress() {
-    getStreak();
-    let temp_streak = parseInt(streak) + 1;
-    if(temp_streak <= 10) {
-      setPoints(points + temp_streak);
-    } else {
-      setPoints(points + Math.pow(temp_streak, 2));
+  const onPress = async () => {
+    setActive(false);
+    messaging().getToken().then(rtrn => console.log(rtrn));
+    var obj;
+    try {
+      await fetch('https://qrtybatu2l.execute-api.us-east-1.amazonaws.com/press?body=' + username);
+      await fetch('https://qrtybatu2l.execute-api.us-east-1.amazonaws.com/fetch/self?username=' + username)
+        .then(res => res.json())
+        .then(data => obj = data)
+        .then(() => console.log(obj));
+
+      setPoints(obj['score']);
+      setStreak(obj['streak']);
+    } catch (error) {
+      console.log(error);
     }
-    set(temp_streak.toString());   
   }
 
+  //Custom button component
   const AppButton = ({ onPress, title }) => {
     return (
       <TouchableHighlight 
+        disabled = {!active}
         activeOpacity={.8}
         underlayColor="#db0000"
         onPress={onPress}
-        style={styles.appButtonContainer}
+        style={active ? styles.enabled : styles.disabled}
       >
-        <Text style={styles.appButtonText}>{title}</Text>
+        <Text style={styles.appButtonText}>{active ? title : "Disabled"}</Text>
       </TouchableHighlight>
     );
   }
 
-  //Get the streak value before return so it prints the correct streak
-  getStreak();
   return (
-    <View style = {styles.container}>
-      <AppButton onPress={onPress} title="Button Party"/>
-      <Text style={styles.bottomText}>Points: {points}</Text>
-      <Text style={styles.bottomText}>Streak: {streak}</Text>
-      <Button style={styles.buttomButton} onPress={clear} title="Clear"/>
-      <Button style={styles.buttomButton} onPress={() => setPoints(0)} title="Reset Points"/>
+    <View style={styles.container}>
+      <View style={styles.topText}>
+        <Text style={styles.Title}>BUTTON PARTY</Text>
+        <Text style={styles.Scores}>
+          <Text style={styles.points}>Points: {points}                                                </Text>
+          <Text style={styles.streak}>Streak: {streak}</Text>
+        </Text>
+      </View>
+      <View style={styles.button}>
+        <AppButton onPress={onPress} title="Press"/>
+      </View>
+      <View style={styles.tempButtons}>
+        <Button style={styles.buttomButton} onPress={() => setStreak(0)} title="Clear"/>
+        <Button style={styles.buttomButton} onPress={() => setPoints(0)} title="Reset Points"/>
+        <Button style={styles.buttomButtom} onPress={() => setActive(!active)} title="Button ON/OFF"/>
+        <Button title="Display Notification" onPress={() => onMessageReceived()} />
+      </View>
+      <Text>Active: {active ? "true" : "false"}</Text>
     </View>
 );
 }
@@ -82,32 +116,62 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#c2c2c2',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#c2c2c2',
+    justifyContent: 'space-between'
   },
-  appButtonContainer: {
-    backgroundColor: "#ff0000",
-    width: 390,
-    height: 390,
+  topText: {
+    flex: .15,
+    alignItems: 'center',
+    textTransform: 'uppercase',
+  },
+  Title: {
+    fontSize: 30,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  Scores: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  button: {
+    flex: .55,
+  },
+  tempButtons: {
+    flex: .3,
+  },
+  enabled: {
+    backgroundColor: '#ff0000',
+    width: 350,
+    height: 350,
     borderRadius: 390,
     alignItems: 'center',
     justifyContent: 'center',
-    elevation: 8,
+    elevation: 10,
+  },
+  disabled: {
+    backgroundColor: '#db0000',
+    width: 350,
+    height: 350,
+    borderRadius: 390,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 10,
+  },
+  appButtonContainer: {
+    width: 350,
+    height: 350,
+    borderRadius: 390,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 10,
   },
   appButtonText: {
     fontSize: 30,
-    color: "#000",
-    fontWeight: "bold",
-    alignSelf: "center",
-    textTransform: "uppercase"
-  },
-  bottomText: {
-    paddingTop: 50,
-    fontSize: 18,
-    color: "#000",
-    fontWeight: "bold",
-    alignSelf: "center",
-    textTransform: "uppercase"
+    color: '#000',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase'
   },
 });
