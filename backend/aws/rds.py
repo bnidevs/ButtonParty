9a8a6b1b4ee8e217690999e030345d0d2cd6396d
pinkoa2 <pinkoa2.rpi.edu>
2021-11-20 17:32:04 -0500

    add user extender table

--- backend/aws/rds.py
+++ backend/aws/rds.py
@@ -18,68 +18,41 @@ def rds_execute_statement( sql, parameters ):
         )
 
 def set_late_users_streak_to_zero():
-    return RDS_CLIENT.execute_statement(
-        continueAfterTimeout = True,
-        resourceArn = RDS_RESOURCE_ARN,
-        secretArn = RDS_SECRET_ARN,
-        database = DATABASE_NAME,
-        sql = SET_LATE_USERS_STREAK_TO_ZERO_SQL
-    )
+    return rds_execute_statement( SET_LATE_USERS_STREAK_TO_ZERO_SQL, [])
 
 def set_pressed_to_false_for_all():
-    return RDS_CLIENT.execute_statement(
-        continueAfterTimeout = True,
-        resourceArn = RDS_RESOURCE_ARN,
-        secretArn = RDS_SECRET_ARN,
-        database = DATABASE_NAME,
-        sql = 'UPDATE ButtonParty SET pressed = False'
-    )
+    sql = 'UPDATE ButtonParty SET pressed = False'
+    return rds_execute_statement( sql, [] )
 
 def add_user_to_RDS(username=random.randint(1000000,9999999)):
-    RDS_CLIENT.execute_statement(
-        continueAfterTimeout = True,
-        resourceArn = RDS_RESOURCE_ARN,
-        secretArn = RDS_SECRET_ARN,
-        database = DATABASE_NAME,
-        sql = 'INSERT INTO ButtonParty (username, score, streak) VALUES (:new_username, :new_score, :new_streak)',
-        parameters = [
-            {'name': 'new_username', 'value': {'stringValue': str(username)}},
-            {'name': 'new_score', 'value': {'longValue': 0}},
-            {'name': 'new_streak', 'value': {'longValue': 0}}
-            ]
-    )
-    RDS_CLIENT.execute_statement(
-        continueAfterTimeout = True,
-        resourceArn = RDS_RESOURCE_ARN,
-        secretArn = RDS_SECRET_ARN,
-        database = DATABASE_NAME,
-        sql = 'INSERT INTO FreezePowerUp VALUES(:new_username, NULL)',
-        parameters = [
-            {'name': 'new_username', 'value': {'stringValue': str(username)}}
-            ]
-    )
-    RDS_CLIENT.execute_statement(
-        continueAfterTimeout = True,
-        resourceArn = RDS_RESOURCE_ARN,
-        secretArn = RDS_SECRET_ARN,
-        database = DATABASE_NAME,
-        sql = 'INSERT INTO MultiplierPowerUp VALUES(:new_username, NULL, 1)',
-        parameters = [
-            {'name': 'new_username', 'value': {'stringValue': str(username)}}
-            ]
-    )
+
+    # Main Table
+    sql = 'INSERT INTO ButtonParty (username, score, streak) VALUES (:new_username, :new_score, :new_streak)'
+    parameters = [
+        {'name': 'new_username', 'value': {'stringValue': str(username)}},
+        {'name': 'new_score', 'value': {'longValue': 0}},
+        {'name': 'new_streak', 'value': {'longValue': 0}}
+    ]
+    rds_execute_statement(sql, parameters)
+
+    # Freeze table
+    sql = 'INSERT INTO FreezePowerUp VALUES(:new_username, NULL)'
+    rds_execute_statement(sql, parameters)
+
+    # Multiplier Table
+    sql = 'INSERT INTO MultiplierPowerUp VALUES(:new_username, NULL, 1)'
+    rds_execute_statement(sql, parameters)
+
+    # Extender Table
+    sql = 'INSERT INTO ExtenderPowerUp VALUES(:new_username, NULL, 1)'
+    rds_execute_statement(sql, parameters)
 
 def get_user_from_RDS(username):
-    response = RDS_CLIENT.execute_statement(
-        continueAfterTimeout = True,
-        resourceArn = RDS_RESOURCE_ARN,
-        secretArn = RDS_SECRET_ARN,
-        database = DATABASE_NAME,
-        sql = f'SELECT * FROM ButtonParty WHERE username=:input_username;',
-        parameters = [
-            {'name': 'input_username', 'value': {'stringValue': str(username)}}
-            ]
-        )['records'][0]
+    sql = 'SELECT * FROM ButtonParty WHERE username=:input_username;'
+    parameters = [
+        {'name': 'input_username', 'value': {'stringValue': str(username)}}
+    ]
+    response = rds_execute_statement(sql, parameters)['records'][0]
     return {
         'username': response[0]['stringValue'],
         'score': response[1]['longValue'],
@@ -88,30 +61,19 @@ def get_user_from_RDS(username):
     }
 
 def update_user_in_RDS(username, score, streak):
-    return RDS_CLIENT.execute_statement(
-        continueAfterTimeout = True,
-        resourceArn = RDS_RESOURCE_ARN,
-        secretArn = RDS_SECRET_ARN,
-        database = DATABASE_NAME,
-        sql = f'UPDATE ButtonParty SET score=:new_score, streak=:new_streak, pressed = True WHERE username=:input_username;',
-        parameters = [
-            {'name': 'input_username', 'value': {'stringValue': str(username)}},
-            {'name': 'new_score', 'value': {'longValue': score}},
-            {'name': 'new_streak', 'value': {'longValue': streak}}
-            ]
-        )
+    sql = 'UPDATE ButtonParty SET score=:new_score, streak=:new_streak, pressed = True WHERE username=:input_username;'
+    parameters = [
+        {'name': 'input_username', 'value': {'stringValue': str(username)}},
+        {'name': 'new_score', 'value': {'longValue': score}},
+        {'name': 'new_streak', 'value': {'longValue': streak}}
+    ]
+    return rds_execute_statement(sql, parameters)
 
 def get_multiplier_of_user(username):
-    response = RDS_CLIENT.execute_statement(
-        continueAfterTimeout = True,
-        resourceArn = RDS_RESOURCE_ARN,
-        secretArn = RDS_SECRET_ARN,
-        database = DATABASE_NAME,
-        sql = GET_MULTIPLER_OF_USER,
-        parameters = [
-            {'name': 'input_username', 'value': {'stringValue': str(username)}}
-        ]
-    )['records']
+    parameters = [
+        {'name': 'input_username', 'value': {'stringValue': str(username)}}
+    ]
+    response = rds_execute_statement(GET_MULTIPLER_OF_USER, parameters)['records']
     if(response):
         return response[0][0]['longValue']
     else:
